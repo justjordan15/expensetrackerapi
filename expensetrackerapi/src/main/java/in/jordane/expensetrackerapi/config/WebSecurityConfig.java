cd6ce15e52af82f8284abca0e171e43e55681efb
justjordan15 <bytedistrict@gmail.com>
2024-11-26 15:57:25 -0500

    Get UserDetails from repo (create custom user details - email login)

--- expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,15 +1,26 @@
 package in.jordane.expensetrackerapi.config;
 
 
+import in.jordane.expensetrackerapi.Security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,12 +36,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("jordy").password("12345").authorities("admin")
-                .and()
-                .withUser("jordan123").password("12345").authorities("user")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.userDetailsService(userDetailsService);
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
