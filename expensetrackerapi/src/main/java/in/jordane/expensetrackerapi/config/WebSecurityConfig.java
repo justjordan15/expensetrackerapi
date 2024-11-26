a6b79d799ebe30894aa04b4cb5a1162e923e852b
justjordan15 <bytedistrict@gmail.com>
2024-11-26 14:44:20 -0500

    Configure multiple user auth

--- expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
@@ -2,8 +2,11 @@ package in.jordane.expensetrackerapi.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,6 +20,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jordy").password("12345").authorities("admin")
+                .and()
+                .withUser("jordan123").password("12345").authorities("user")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
     }
 }
