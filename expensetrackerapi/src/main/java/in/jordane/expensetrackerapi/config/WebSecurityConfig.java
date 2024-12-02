cf78f67192114756e11086e5c7f9c5cf111967f3
justjordan15 <bytedistrict@gmail.com>
2024-12-02 09:54:18 -0500

    Register filter to config file

--- expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/in/jordane/expensetrackerapi/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package in.jordane.expensetrackerapi.config;
 
 
 import in.jordane.expensetrackerapi.Security.CustomUserDetailsService;
+import in.jordane.expensetrackerapi.Security.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +12,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public JwtRequestFilter authenticationJwtTokenFilter(){
+        return new JwtRequestFilter();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,7 +40,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                http.httpBasic();
 
     }
 
